7d4a1413fe8e1ad433e5aab8b466fddd173a783f
Svatoi <privitvlad00@gmail.com>
2026-07-11 17:21:03 +0200

    Assembly memory mapping, zero cope parser, thread pool in main.cpp

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <string_view>
+#include <vector>
+#include <unordered_map>
+#include <chrono>
+#include <algorithm>
+
 #include "MemoryMappedFile.h"
 #include "CsvParser.h"
+#include "ThreadPool.h"
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -10,31 +16,93 @@ int main(int argc, char* argv[]) {
     }
 
     try {
+        auto start_time = std::chrono::high_resolution_clock::now();
+
         MemoryMappedFile mmapFile(argv[1]);
         std::string_view fileView(mmapFile.data(), mmapFile.size());
 
-        size_t firstNewline = fileView.find('\n');
-        if (firstNewline == std::string_view::npos) return 0;
+        size_t headerEnd = fileView.find('\n');
+        if (headerEnd == std::string_view::npos) return 0;
+        std::string_view dataView = fileView.substr(headerEnd + 1);
 
-        size_t secondNewline = fileView.find('\n', firstNewline + 1);
-        if (secondNewline == std::string_view::npos) return 0;
+        unsigned int num_threads = std::thread::hardware_concurrency();
+        size_t chunkSize = dataView.size() / num_threads;
 
-        std::string_view firstDataRow = fileView.substr(
-            firstNewline + 1,
-            secondNewline - firstNewline - 1
-        );
+        std::vector<std::string_view> chunks;
+        size_t current_start = 0;
 
-        std::cout << "Raw row: " << firstDataRow << "\n\n";
+        for (unsigned int i = 0; i < num_threads; i++) {
+            size_t current_end = current_start + chunkSize;
 
-        CsvRow row(firstDataRow);
+            if (i == num_threads - 1 || current_end >= dataView.size()) {
+                current_end = dataView.size();
+            } else {
+                size_t nl = dataView.find('\n', current_end);
+                if (nl != std::string_view::npos) {
+                    current_end = nl + 1;
+                } else {
+                    current_end = dataView.size();
+                }
+            }
+            chunks.push_back(dataView.substr(current_start, current_end - current_start));
+            current_start = current_end;
+        }
 
-        std::cout << "--- Parsed Data ---\n";
-        std::cout << "Transaction ID : " << row.getString(0) << "\n";
-        std::cout << "User ID        : " << row.getInt(2) << "\n";
-        std::cout << "Amount         : " << row.getDouble(3) << "\n";
-        std::cout << "Currency       : " << row.getString(4) << "\n";
-        std::cout << "Status         : " << row.getString(5) << "\n";
+        std::vector<std::unordered_map<int, double>> localResults(num_threads);
 
+        std::cout << "Processing " << mmapFile.size() / (1024*1024) 
+                << " MB across " << num_threads << " threads...\n";
+
+        {
+            ThreadPool pool(num_threads);
+            for (unsigned int i = 0; i < num_threads; i++) {
+                pool.enqueue([i, chunk=chunks[i], &localMap=localResults[i]] {
+                    size_t pos = 0;
+                    while (pos < chunk.size()) {
+                        size_t endLine = chunk.find('\n', pos);
+                        if (endLine == std::string_view::npos) endLine = chunk.size();
+
+                        std::string_view rowStr = chunk.substr(pos, endLine - pos);
+                        pos = endLine + 1;
+
+                        if (rowStr.empty() || rowStr == "\r") continue;
+
+                        CsvRow row(rowStr);
+
+                        if (row.getString(5) == "SUCCESS") {
+                            int userId = row.getInt(2);
+                            double amount = row.getDouble(3);
+                            localMap[userId] += amount;
+                        }
+                    }
+                });
+            }
+        }
+
+        std::unordered_map<int, double> globalTotals;
+        for (const auto& localMap : localResults) {
+            for (const auto& [userId, amount] : localMap) {
+                globalTotals[userId] += amount;
+            }
+        }
+
+        auto end_time = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> duration = end_time - start_time;
+
+        std::vector<std::pair<int, double>> topUsers(globalTotals.begin(), globalTotals.end());
+
+        std::sort(topUsers.begin(), topUsers.end(),
+        [](const auto& a, const auto& b) {return a.second > b.second; });
+
+        std::cout << "========================================\n";
+        std::cout << "Done in: " << duration.count() << " seconds\n";
+        std::cout << "Unique users with SUCCESS: " << globalTotals.size() << "\n";
+        std::cout << "========================================\n";
+        std::cout << "Top 5 Users:\n";
+
+        for (size_t i = 0; i < std::min<size_t>(5, topUsers.size()); i++) {
+            std::cout << "User " << topUsers[i].first << ": " << topUsers[i].second << "\n";
+        }
     } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return 1;
